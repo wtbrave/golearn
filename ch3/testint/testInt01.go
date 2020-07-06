33136e72f21364bc0ecddb3ae39ad7d414028895
johnny <wtbrave@gmail.com>
2020-07-06 13:41:59 +0800

    int

--- ch3/testint/testInt01.go
+++ ch3/testint/testInt01.go
@@ -13,3 +13,28 @@ func main() {
 
 }
 
+func Plus () {
+	var a uint8 = 255;
+	fmt.Println(a, a+1, a*a);
+}
+
+func BitOperate () {
+	var x uint8 = 1<<1 | 1<<5
+	var y uint8 = 1<<1 | 1<<2
+	fmt.Println(x, y)
+
+	fmt.Printf("%08b\n", x)
+	fmt.Printf("%08b\b", y)
+
+	fmt.Printf("%08b\n", x&y)
+	fmt.Printf("%08b\n", x|y)
+	fmt.Printf("%08b\n", x^y)
+	fmt.Printf("%08b\n", x&^y)
+	fmt.Println(x, y)
+
+
+	var  a uint8 = 12
+	var  b uint8 = 48
+	fmt.Println((a<<2)==b)
+}
+
